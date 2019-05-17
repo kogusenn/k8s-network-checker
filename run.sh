9bd4a84443c622fc56835ef141eac8f9f6783f61
Etienne Tremel <etienne@kogusenn.com>
2019-05-17 14:51:26 +0200

    Do not test pods that share their IP with the host

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ INTERVAL=${INTERVAL:-600}
 
 set -e
 
-namespaces=$(kubectl get namespaces -o custom-columns='NAME:.metadata.name' --no-headers)
+namespaces=$(kubectl get namespaces -o custom-columns='NAME:.metadata.name' --no-headers 2>/dev/null)
 
 while true
 do
   echo "`date` === Network check started"
   for namespace in ${namespaces[@]}
   do
-    service_list=$(kubectl get services -n $namespace -o custom-columns='NAME:.metadata.name,CLUSTER-IP:.spec.clusterIP,PORT(S):.spec.ports[*].port' --no-headers)
+    service_list=$(kubectl get services -n $namespace -o custom-columns='NAME:.metadata.name,CLUSTER-IP:.spec.clusterIP,PORT(S):.spec.ports[*].port' --no-headers 2>/dev/null)
     while read service
     do
       service_name=$(echo $service | awk '{print $1}')
@@ -22,7 +22,7 @@ do
       # Check service connectivity
       if [ "$service_ip" != "None" ]
       then
-        endpoint=$(kubectl get ep -n $namespace $service_name --no-headers | awk '{print $2}')
+        endpoint=$(kubectl get ep -n $namespace $service_name --no-headers 2>/dev/null | awk '{print $2}')
         if [ "${endpoint}" != "" -a "${endpoint}" != "<none>" ]
         then
           for service_port in ${service_port_list[@]}
@@ -54,25 +54,32 @@ do
 
     #
     # Check pod connectivity
-    endpoint_list=$(kubectl get ep -n ${namespace} -o custom-columns='NAME:.metadata.name,IP(S):.subsets[*].addresses[*].ip,PORT(S):.subsets[*].ports[*].port' --no-headers)
+    endpoint_list=$(kubectl get ep -n ${namespace} -o custom-columns='NAME:.metadata.name,IP(S):.subsets[*].addresses[*].ip,PORT(S):.subsets[*].ports[*].port,TARGET:.subsets[*].addresses[*].targetRef.name' --no-headers 2>/dev/null)
     while read endpoint
     do
       pod_ip_list=$(echo $endpoint | awk '{print $2}' | sed 's/,/ /g')
       pod_port_list=$(echo $endpoint | awk '{print $3}' | sed 's/,/ /g')
+      pod_name_list=$(echo $endpoint | awk '{print $4}' | sed 's/,/ /g')
 
-      if [ "$pod_ip_list" != "<none>" -a "$pod_port_list" != "<none>" ]
+      if [ "$pod_ip_list" != "<none>" -a "$pod_port_list" != "<none>" -a "$pod_name_list" != "<none>" ]
       then
+        index=0
         for pod_ip in ${pod_ip_list[@]}
         do
+          pod_name=${pod_name_list[$count]}
+          index=$((index+1))
           for pod_port in ${pod_port_list[@]}
           do
-            pod_name=$(kubectl get po -n ${namespace} -o wide | grep -v Error | grep " $pod_ip " | awk '{print $1}')
-            echo -n "[POD CONNECT] - ${namespace}/${pod_name} (${pod_ip}:${pod_port}) "
-            if nc -z -w 1 $pod_ip $pod_port > /dev/null
+            pod_host_ip=$(kubectl get po -n ${namespace} ${pod_name} -o jsonpath='{.status.hostIP}' 2>/dev/null)
+            if [ "$pod_ip" != "$pod_host_ip" ]
             then
-              echo "[OK]"
-            else
-              echo "[FAILED]"
+              echo -n "[POD CONNECT] - ${namespace}/${pod_name} (${pod_ip}:${pod_port}) "
+              if nc -z -w 1 $pod_ip $pod_port > /dev/null
+              then
+                echo "[OK]"
+              else
+                echo "[FAILED]"
+              fi
             fi
           done
         done
